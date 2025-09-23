ef9d814e838d8faa66485905fd0f717e88dfbedd
0xf1e <fie@0xf1e.dev>
2025-09-23 07:55:28 +0200

    feat: pipeline.sh runs podman kube play

--- pipeline.sh
+++ pipeline.sh
@@ -2,14 +2,6 @@ mkdir -p .local/raw_data .local/metrics
 
 chmod o+w .local/raw_data .local/metrics
 
-podman run --rm \
-  -e GITHUB_TOKEN_FILE=/app/token.txt \
-  -v ./.token:/app/token.txt \
-  -v $(pwd)/.local/raw_data:/app/output \
-  codeberg.org/0xf1e/project-health-analyzer:latest
+podman kube play pipeline.yaml
 
-podman run --rm -it --name duckdb-importer \
-  -v "$(pwd)"/.local/raw_data:/raw_data \
-  -v "$(pwd)"/.local/metrics:/metrics \
-  -v "$(pwd)"/pipeline.sql:/app/pipeline.sql \
-  docker.io/duckdb/duckdb duckdb /metrics/project_health.db -f /app/pipeline.sql
+podman kube down pipeline.yaml
